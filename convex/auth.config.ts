eadf7a56b80efacfa836018cd9001125ebebb49a
henryhoangduong <henryhoangduong@gmail.com>
2025-07-09 21:13:25 +0700

    ConvexClientProvider

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://assured-mouse-51.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/providers/convex-client-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+interface ConvexClientProviderProps{
+    children: React.ReactNode
+}
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!
+
+const convex = new ConvexReactClient(convexUrl)
+
+export const ConvexClientProvider = ({ children }: ConvexClientProviderProps)=>{
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
