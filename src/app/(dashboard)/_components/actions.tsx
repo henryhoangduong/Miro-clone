af34a2d18ab86c551fbe65959e77d4203f952f62
henryhoangduong <henryhoangduong@gmail.com>
2025-07-12 00:10:29 +0700

    alert dialog

--- src/app/(dashboard)/_components/actions.tsx
+++ src/app/(dashboard)/_components/actions.tsx
@@ -11,6 +11,8 @@ import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "../../../../convex/_generated/api";
+import { ConfirmModal } from "./confirm-modal";
+import { Button } from "@/components/ui/button";
 interface ActionProps {
   children: React.ReactNode;
   side?: DropdownMenuContentProps["side"];
@@ -27,9 +29,10 @@ export const Actions = ({ children, side, sideOffset, id }: ActionProps) => {
   };
   const { pending, mutate } = useApiMutation(api.board.remove);
   const onDelete = () => {
-    mutate({ id }).then(() => toast.success("Board deleted"))
-    .catch(()=>toast.error("Failed to delete board"))
-  }
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
@@ -44,10 +47,20 @@ export const Actions = ({ children, side, sideOffset, id }: ActionProps) => {
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={onDelete} className="p-3 cursor-pointer">
-          <Trash2 className="h-4 w-4 mr-2" />
-          Delete
-        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete boards?"
+          description="This will delete the board and all of the contents"
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+            <Button
+              variant={"ghost"}
+              className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+            >
+              <Trash2 className="h-4 w-4 mr-2" />
+              Delete
+            </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
